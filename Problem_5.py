268ccd478bf106cc97e5453386426997f37a9afe
MarianaDTU <100677870+Marianadesamadsen@users.noreply.github.com>
2025-09-30 13:41:40 +0200

    problem 5

--- Problem_5.py
+++ Problem_5.py
@@ -2,6 +2,7 @@ import numpy as np
 from src.FourTankSystem import FourTankSystem
 import params.parameters_tank as para
 import matplotlib.pyplot as plt
+from numpy.linalg import eig, inv, solve
 
 t0 = 0
 tf = 20*60 
@@ -21,8 +22,18 @@ R_s = np.eye(4)*0.1
 R_d = np.eye(2)*0.1
 delta_t = 1
 
+
+# Linearize continous time
 Model_Deterministic = FourTankSystem(R_s*0, R_d*0, p, delta_t)
+xs = Model_Deterministic.GetSteadyState(x0, u, d)
+Ac,Bc,C,Cz = Model_Deterministic.LinearizeContinousTime(xs,d)
+print(Ac)
+print(Bc)
+print(C)
+print(Cz)
 
-
+poles = eig(Ac)[0]
+print("Poles:",poles,"\nSystem stable:",np.all(poles<0))
+G_transfer = lambda s: C @ np.linalg.inv(s*np.eye(Ac.shape[0]) - Ac) @ Bc
 
 
