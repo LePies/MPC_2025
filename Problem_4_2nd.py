64f975848fd0c4924367ab1f06e4cd6bfe7d3883
Oliver Mohr <oliver@mohr.dk>
2025-11-06 16:05:57 +0100

    QPSolver.py updated

--- Problem_4_2nd.py
+++ Problem_4_2nd.py
@@ -297,11 +297,11 @@ plt.close()
 
 # Estimate system order based on singular values
 # Look for a significant drop in singular values
-threshold = 1e-17/S[0]  # 1% of the largest singular value
+threshold = 1e-17  # 1% of the largest singular value
 significant_singular_values = S[S > threshold * S[0]]
 estimated_order = len(significant_singular_values)
 
-N = estimated_order
+N = 6
 
 S_sqrt = np.diag(np.sqrt(S[:N]))
 
